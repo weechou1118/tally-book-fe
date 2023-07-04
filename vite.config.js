e6ba42bccad21cf32857d7d698ae2dd3d75550cd
zhouwei <weechou999@gmail.com>
2023-07-04 11:10:39 +0800

    feat: 配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
+import { createStyleImportPlugin } from 'vite-plugin-style-import'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [react(), createStyleImportPlugin(
+    {
+      libs: [
+        {
+          libraryName: 'zarm',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `zarm/es/${name}/style/css`
+          }
+        }
+      ]
+    }
+  )],
+  css: {
+    modules: {
+      localsConvention: 'dashesOnly',
+      preprocessorOptions: {
+        less: {
+          // 支持内联 JavaScript
+          javascriptEnabled: true,
+        }
+      }
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:7001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'utils': path.resolve(__dirname, 'src/utils')
+    }
+  }
 })
