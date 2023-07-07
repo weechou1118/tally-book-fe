9808ce9f816d7f16d55fed47b1eeadbe69c2b320
zhouwei <weechou999@gmail.com>
2023-07-07 10:25:53 +0800

    conf: 添加自动化部署配置

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'tally_book',
+      script: 'vite-h5-server.js'
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '121.199.27.121',
+      ref: 'origin/main',
+      repo: 'git@github.com:weechou1118/tally-book-fe.git',
+      path: '/root/workspace/tally_book',
+      'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+}
